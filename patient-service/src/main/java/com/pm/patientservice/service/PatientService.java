60e0d29260ae2eef9144a1cbb088cecceff7cd99
EzharAnsari <ea199100@gmail.com>
2026-07-05 10:41:03 +0530

    delete-patient

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -65,4 +65,8 @@ public class PatientService {
         Patient updatedPatient = patientRepository.save(patient);
         return PatientMapper.toDTO(updatedPatient);
     }
+
+    public void deletePatient(UUID id) {
+        patientRepository.deleteById(id);
+    }
 }
