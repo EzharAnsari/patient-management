11b78c9b0fac14ae201b564e6620a8c32cb1b142
EzharAnsari <ea199100@gmail.com>
2026-07-05 18:02:24 +0530

    implementing-grpc-server-in-billing-service

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,31 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest,
+         StreamObserver<BillingResponse> responseStreamObserver) {
+
+        log.info("CreateBillingAccount request received {}", billingRequest.toString());
+
+        // Business logic - e.g save to database, perform calculates etc
+
+        BillingResponse billingResponse = BillingResponse.newBuilder()
+            .setAccountId("123123")
+            .setStatus("ACTIVE")
+            .build();
+
+        responseStreamObserver.onNext(billingResponse);
+        responseStreamObserver.onCompleted();
+    }
+}
